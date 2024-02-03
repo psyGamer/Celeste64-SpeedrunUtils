ed8e89a37438f9384e757c6dd65012bf494c2358
psyGamer <psygamer21@gmail.com>
2024-02-03 22:47:28 +0100

    Port CI pipeline from TAS tools

--- Source/Game.cs
+++ Source/Game.cs
@@ -41,8 +41,8 @@ public class Game : Module
 	public const int Width = 640;
 	public const int Height = 360;
 	public static readonly Version Version = typeof(Game).Assembly.GetName().Version!;
-    public static readonly Version SpeedrunUtilsVersion = new Version(0, 2, 2);
-	public static readonly string VersionString = $"v.{Version.Major}.{Version.Minor}.{Version.Build} - SpeedrunUtils v.{SpeedrunUtilsVersion.Major}.{SpeedrunUtilsVersion.Minor}.{SpeedrunUtilsVersion.Build}";
+    public static string TASVersion => string.IsNullOrEmpty(BuildProperties.ModVersion()) ? "dev" : BuildProperties.ModVersion();
+    public static string VersionString => $"v.{Version.Major}.{Version.Minor}.{Version.Build} - SpeedrunUtils v.{TASVersion}";
 
 	/// <summary>
 	/// Used by various rendering elements to proportionally scale if you change the default game resolution
